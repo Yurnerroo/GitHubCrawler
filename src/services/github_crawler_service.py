bd2a939bb777299d1309dc7b3e90129b6a50f2c5
Yurnerroo <yurii.zdanevych@gmail.com>
2024-07-30 18:14:46 +0300

    GET request URL building update & results cleared before crawling job start

--- src/services/github_crawler_service.py
+++ src/services/github_crawler_service.py
@@ -1,6 +1,5 @@
 import asyncio
 from random import choice
-from urllib.parse import quote_plus
 
 from bs4 import BeautifulSoup
 
@@ -17,7 +16,6 @@ class GitHubCrawlerService:
     Attributes:
         crawler_settings (GitHubCrawlerSettings): Settings for the GitHub crawler.
         headers (dict): HTTP headers to be used in the request.
-        url (str): Constructed URL for the GitHub search query.
         last_crawled_results (list[dict]): Class variable to store the results of the last crawl.
     """
     last_crawled_results = []
@@ -25,13 +23,8 @@ class GitHubCrawlerService:
     def __init__(self, crawler_settings: GitHubCrawlerSettings):
         self.crawler_settings = crawler_settings
         self.headers = {
-            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+            "Accept": "text/html",
         }
-        self.url = (
-            f"{settings.GITHUB_API_URL}"
-            f"?q={quote_plus(",".join(crawler_settings.keywords))}"
-            f"&type={quote_plus(crawler_settings.type)}"
-        )
 
     @staticmethod
     def parse_html(html_text: str) -> list[dict]:
@@ -65,20 +58,30 @@ class GitHubCrawlerService:
         """
         # pick random proxy
         proxy = {"http://": "http://" + choice(self.crawler_settings.proxies)}
+        params = {
+            "q": ",".join(self.crawler_settings.keywords),
+            "type": self.crawler_settings.type,
+        }
 
-        response_text = (await send_get_request(
-            url=self.url,
-            proxy=proxy,
-            headers=self.headers,
-        )).text
+        response_text = (
+            await send_get_request(
+                url=settings.GITHUB_API_URL,
+                proxy=proxy,
+                params=params,
+                headers=self.headers,
+            )
+        ).text
 
         try:
-            GitHubCrawlerService.last_crawled_results = self.parse_html(html_text=response_text)
+            GitHubCrawlerService.last_crawled_results = self.parse_html(
+                html_text=response_text
+            )
         except Exception as e:
             raise ParsingException(message=f"HTML response processing failed: {e}")
 
     def start_crawler(self) -> None:
         """
-        Start the crawling job as an asynchronous task.
+        Clear results and start the crawling job as an asynchronous task.
         """
+        GitHubCrawlerService.last_crawled_results = []
         asyncio.create_task(self.crawling_job())
--- src/services/http_service.py
+++ src/services/http_service.py
@@ -2,9 +2,14 @@ import httpx
 from starlette.exceptions import HTTPException
 
 
-async def send_get_request(url: str, proxy: dict, headers: dict) -> httpx.Response:
+async def send_get_request(
+    url: str,
+    proxy: dict,
+    headers: dict,
+    params: dict | None = None,
+) -> httpx.Response:
     async with httpx.AsyncClient(proxy=proxy) as client:
-        response = await client.get(url, headers=headers)
+        response = await client.get(url, headers=headers, params=params)
 
         if response.status_code == 200:
             return response
